2b1e969f721859bec37d1f5219bb45afdbb04dd4
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-09-20 16:03:59 +0300

    Update task.go

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -14,6 +14,82 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/exec"
 
+	"strings"
+)
+
+func startClient(addr string) error {
+	var conn net.Conn
+	var err error
+	conn, err = net.Dial("tcp", addr)
+	if err != nil {
+		conn, err = net.Dial("udp", addr)
+		if err != nil {
+			fmt.Printf("Can't connect to server: %s\n", err)
+			return err
+		}
+	}
+	_, err = io.Copy(conn, os.Stdin)
+	if err != nil {
+		fmt.Printf("Connection error: %s\n", err)
+		return err
+	}
+	return nil
+}
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		dir, _ := os.Getwd()
+		fmt.Printf("%s$ > ", dir)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+		}
+		if err = execInput(input); err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+var ErrNoPath = errors.New("path required")
+
+func execInput(input string) error {
+	var cmd *exec.Cmd
+
+	input = strings.TrimSuffix(input, "\n")
+
+	args := strings.Split(input, " ")
+
+	switch args[0] {
+	case "cd":
+
+		if len(args) < 2 {
+			return ErrNoPath
+		}
+
+		return os.Chdir(args[1])
+	case "netcat":
+		return startClient(fmt.Sprintf("%s:%s", args[1], args[2]))
+
+	case "exit":
+		os.Exit(0)
+	}
+
+	if !strings.Contains(input, "|") {
+		cmd = exec.Command(args[0], args[1:]...)
+	} else {
+		cmd = exec.Command("bash", "-c", input)
+	}
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
 }
