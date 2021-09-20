2ea31b35b18a33a4067641b63e84e9ca15b42efd
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-09-20 15:56:12 +0300

    Update task.go

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -19,6 +19,50 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
 
+func anagramma(a ...string) *map[string][]string {
+	var arStringIntAll []string
+	map1 := make(map[string]int)
+	map2 := make(map[string][]string)
+	map3 := make(map[string][]string)
+	for _, v := range a {
+		word := strings.ToLower(v)
+		runes := []rune(word)
+		var arRuneInt []int
+		for _, v1 := range runes {
+			arRuneInt = append(arRuneInt, int(v1))
+		}
+		sort.Ints(arRuneInt)
+		var arStringIntPart []string
+		for _, v2 := range arRuneInt {
+			arStringIntPart = append(arStringIntPart, strconv.Itoa(v2)) //string
+		}
+		myStr := strings.Join(arStringIntPart, "")
+		arStringIntAll = append(arStringIntAll, myStr)
+
+	}
+	for _, v3 := range arStringIntAll {
+		map1[v3]++
+	}
+	for i4, v4 := range map1 {
+		if v4 > 1 {
+			for i5, v5 := range arStringIntAll {
+				if i4 == v5 {
+					map2[i4] = append(map2[i4], strings.ToLower(a[i5]))
+				}
+			}
+		}
+	}
+	for _, t4 := range map2 {
+		map3[t4[0]] = t4
+	}
+	for _, t5 := range map3 {
+		sort.Strings(t5)
+	}
+	return &map3
 }
