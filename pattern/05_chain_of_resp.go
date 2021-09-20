9d545bacafbc43acb4374c33b2e331a47dd2199b
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-09-20 15:32:21 +0300

    Update 05_chain_of_resp.go

--- pattern/05_chain_of_resp.go
+++ pattern/05_chain_of_resp.go
@@ -1,7 +1,61 @@
 package pattern
 
 /*
-	Реализовать паттерн «цепочка вызовов».
-Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Chain-of-responsibility_pattern
+Паттерн цепочка обязанностей позволяет передавать запрос по цепочке потенциальных обработчиков, пока один из них не обработает запрос. 
+Плюсы: избавляет от жёсткой привязки отправителя запроса к его получателю. Минусы: запрос теряется, если не удовлетворяет ни одному обработчику.
 */
+import "fmt"
+
+type Case interface {
+	execute(*Recipe)
+	setNext(Case)
+}
+type Recipe struct {
+	WantRecipe string
+}
+
+type Sister struct {
+	next Case
+	myRecipe string
+}
+func (s *Sister) execute(r *Recipe){
+	if r.WantRecipe==s.myRecipe {fmt.Println("I am sister. Yes, i have this recipe!")} else{s.next.execute(r)}
+}
+func (s *Sister) setNext(next Case) {
+	s.next = next
+}
+type Internet struct{
+	next Case
+	myRecipe string
+}
+func (i *Internet) execute(r *Recipe){
+	if r.WantRecipe==i.myRecipe {fmt.Println("I am internet. Yes, i have this recipe!")} else{fmt.Println("Nobody knows this recipe!:(")}
+}
+func (i *Internet) setNext(next Case) {
+	i.next = next
+}
+
+type Friend struct{
+	next Case
+	myRecipe string
+}
+func (f *Friend) execute(r *Recipe){
+	if r.WantRecipe==f.myRecipe {fmt.Println("I am friend. Yes, i have this recipe!")} else{f.next.execute(r)}
+}
+func (f *Friend) setNext(next Case) {
+	f.next = next
+}
+
+func main(){
+	internet:=&Internet{myRecipe: "Оливье"}
+
+	sister:=&Sister{myRecipe: "Пицца"}
+	sister.setNext(internet)
+
+	friend:=&Friend{myRecipe: "Яичница"}
+	friend.setNext(sister)
+
+	recipe:=&Recipe{WantRecipe: "Оливье"}
+	friend.execute(recipe)
+
+}
