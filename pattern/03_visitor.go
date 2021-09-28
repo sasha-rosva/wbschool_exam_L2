0c649f8b939ed0d2dac7cd2dff4e6cdb17731d62
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-09-28 18:28:12 +0300

    Update 03_visitor.go

--- pattern/03_visitor.go
+++ pattern/03_visitor.go
@@ -4,6 +4,7 @@ package pattern
 Паттерн visitor позволяет добавлять в программу новые операции, не изменяя структуру объектов. Паттерн позволяет применить одну и ту же операцию к различным объектам.
 Плюсы: упрощает добавление операций, работающих со сложными структурами объектов. Минусы: паттерн не оправдан, если иерархия элементов часто меняется.
 */
+
 import "fmt"
 
 type typer interface {
@@ -11,29 +12,29 @@ type typer interface {
 	accept(visitor)
 }
 type visitor interface {
-	visitMap(*Map)
-	visitArray(*Array)
+	visitMap(Map)
+	visitArray(Array)
 }
 type Map struct{ myType string }
 
-func (m *Map) getType() string  { return m.myType }
-func (m *Map) accept(v visitor) { v.visitMap(m) }
+func (m Map) getType() string  { return m.myType }
+func (m Map) accept(v visitor) { v.visitMap(m) }
 
 type Array struct{ myType string }
 
-func (a *Array) getType() string  { return a.myType }
-func (a *Array) accept(v visitor) { v.visitArray(a) }
+func (a Array) getType() string  { return a.myType }
+func (a Array) accept(v visitor) { v.visitArray(a) }
 
 type Slice struct{ myType string }
 
-func (s *Slice) visitMap(m *Map) {
+func (s Slice) visitMap(m Map) {
 	if s.myType != m.myType {
 		fmt.Printf("Наши типы НЕ равны! У тебя %s, а у меня %s!\n", m.myType, s.myType)
 	} else {
 		fmt.Println("Наши типы равны!")
 	}
 }
-func (s *Slice) visitArray(a *Array) {
+func (s Slice) visitArray(a Array) {
 	if s.myType != a.myType {
 		fmt.Printf("Наши типы НЕ равны! У тебя %s, а у меня %s!\n", a.myType, s.myType)
 	} else {
@@ -44,7 +45,7 @@ func (s *Slice) visitArray(a *Array) {
 func main() {
 	map1 := &Map{"Map"}
 	array1 := &Array{"Array"}
-	slice := &Slice{"Slice"} // это visitor
+	slice := &Slice{"Slice"}
 
 	map1.accept(slice)
 	array1.accept(slice)
