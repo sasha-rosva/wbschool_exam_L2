cf3b183ea5c066f203c3ade8d592f56e5215e4ba
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-09-28 12:42:21 +0300

    Update task_test.go

--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -6,20 +6,23 @@ import (
 )
 
 func Test_anagramma(t *testing.T) {
-	itogMap:=make (map[string][]string)
-	a1:=[]string{"кот", "кто", "окт", "ток"}
-	a2:=[]string{"кит", "тик"}
-	a3:=[]string{"крона", "норка"}
-	a4:=[]string{"пятка", "тяпка"}
-	itogMap["ток"]=a1
-	itogMap["тик"]=a2
-	itogMap["норка"]=a3
-	itogMap["тяпка"]=a4
+	itogMap := make(map[string][]string)
+	a1 := []string{"кот", "кто", "окт", "ток"}
+	a2 := []string{"кит", "тик"}
+	a3 := []string{"крона", "норка"}
+	a4 := []string{"пятка", "тяпка"}
+	itogMap["ток"] = a1
+	itogMap["тик"] = a2
+	itogMap["норка"] = a3
+	itogMap["тяпка"] = a4
 
 	t.Run("test", func(t *testing.T) {
-		massiv := &[...]string{"Тяпка", "пятка", "ток", "коТ", "окт", "тик", "кИт", "Яблоко", "КТО", "НОРКа", "КроНА"}
-			gotMap := anagramma(massiv[:]...)
-		for o, oo:= range *gotMap {
-			if  !reflect.DeepEqual(oo, itogMap[o]) {
+		massiv := &[]string{"Тяпка", "пятка", "ток", "коТ", "окт", "тик", "кИт", "Яблоко", "КТО", "НОРКа", "КроНА"}
+		gotMap := anagramma(massiv)
+		for o, oo := range *gotMap {
+			if !reflect.DeepEqual(oo, itogMap[o]) {
 				t.Errorf("anagramma() = %v, want %v", gotMap, itogMap)
-			}}})}
+			}
+		}
+	})
+}
