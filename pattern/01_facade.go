348356a425e6d0b6f8c83f986981892027a7c267
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-09-28 18:21:10 +0300

    Update 01_facade.go

--- pattern/01_facade.go
+++ pattern/01_facade.go
@@ -3,15 +3,16 @@ package pattern
 Паттерн используется, когда нужно предоставить простой интерфейс к сложной подсистеме.
 Пример: "Умный дом"
 */
+
 import (
 	"fmt"
 	"time"
 )
 
 type SmartHouseFacade struct {
-	Water *Water
-	Light *Light
-	Tv    *Tv
+	Water Water
+	Light Light
+	Tv    Tv
 }
 type Water struct {}
 type Light struct {}
@@ -19,34 +20,34 @@ type Tv struct {}
 func NewSmartHouseFacade()*SmartHouseFacade{
 	return &SmartHouseFacade{}
 }
-func (w *SmartHouseFacade) IAmHome(){
+func (w SmartHouseFacade) IAmHome(){
 	fmt.Println("Запущен режим: 'Я дома!'")
 	w.Water.TurnOnWater()
 	w.Light.SwitchOnLight()
 	w.Tv.ActivateTv()
 }
-func (w *SmartHouseFacade) IGoAway(){
+func (w SmartHouseFacade) IGoAway(){
 	fmt.Println("Запущен режим: 'Я ушел!'")
 	w.Water.TurnOffWater()
 	w.Light.SwitchOffLight()
 	w.Tv.DeactivateTv()
 }
-func (w *Water) TurnOnWater(){
+func (w Water) TurnOnWater(){
 	fmt.Println("Вода включена!")
 }
-func (w *Water) TurnOffWater(){
+func (w Water) TurnOffWater(){
 	fmt.Println("Вода выключена!")
 }
-func (w *Light) SwitchOnLight(){
+func (w Light) SwitchOnLight(){
 	fmt.Println("Свет включен!")
 }
-func (w *Light) SwitchOffLight(){
+func (w Light) SwitchOffLight(){
 	fmt.Println("Свет выключен!")
 }
-func (w *Tv) ActivateTv(){
+func (w Tv) ActivateTv(){
 	fmt.Println("Телевизор включен!")
 }
-func (w *Tv) DeactivateTv(){
+func (w Tv) DeactivateTv(){
 	fmt.Println("Телевизор выключен!")
 }
 func main(){
